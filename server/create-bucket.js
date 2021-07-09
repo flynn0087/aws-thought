94001a5a23f2cd708e5852756f3f4e66d130d616
Ben Flynn <flynn0087@gmail.com>
2021-07-09 12:57:32 -0500

    call s3 to create bucket added

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -12,3 +12,12 @@ const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 var bucketParams = {
     Bucket : "user-images-" + uuidv4();
 };
+
+//call S3 to create the bucket
+s3.createBucket(bucketParams, (err, data) => {
+    if (err) {
+        console.log("Error", err); 
+    } else {
+        console.log("Success");
+    }
+});
