4302c8d7f682a0eac9433f8597e98c66fc49409c
Ben Flynn <flynn0087@gmail.com>
2021-07-09 12:55:07 -0500

    bucket params added

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -8,3 +8,7 @@ AWS.config.update({region: 'us-east-2'});
 // Create S3 service object
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
+//Create the parameters for calling createBucket
+var bucketParams = {
+    Bucket : "user-images-" + uuidv4();
+};
