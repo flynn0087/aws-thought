d4bb45201de90f4f5aee746d2fd42a5525919a0e
Ben Flynn <flynn0087@gmail.com>
2021-07-10 12:26:39 -0500

    loading seed file created, dynamoDB config added

--- client/server/db/LoadThoughts.js
+++ client/server/db/LoadThoughts.js
@@ -1,3 +1,10 @@
 // dependencies
 const AWS = require("aws-sdk");
 const fs = require('fs');
+
+//dyanmoDB config
+AWS.config.update({
+    region: "us-east-2",
+    endpoint: "http://localhost:8000"
+});
+const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
