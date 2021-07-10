5b7c65922a1559d0dc32bd9c3ce21679baa6b734
Ben Flynn <flynn0087@gmail.com>
2021-07-10 12:03:41 -0500

    table parameters added

--- client/server/db/CreateThoughtsTable.js
+++ client/server/db/CreateThoughtsTable.js
@@ -10,3 +10,19 @@ AWS.config.update({
 //dynamoDB service object
 const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
+// parameters of table that hold schema and metadata
+const params = {
+    TableName : "Thoughts",
+    KeySchema: [
+        { AttributeName: "username", KeyType: "HASH" }, // partition key
+        { AttributeName: "createdAt", KeyType: "RANGE" } // sort key
+    ],
+    AttributeDefinitions: [
+        { AttributeName: "username", AttributeType: "S" },
+        { AttributeName: "createdAt", AttributeType: "N" }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+};
