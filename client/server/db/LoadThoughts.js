5cfff9d3327e6fec6c9fcca3c478ecc49aa2db5d
Ben Flynn <flynn0087@gmail.com>
2021-07-10 12:28:45 -0500

    fs utilization to read users.json added

--- client/server/db/LoadThoughts.js
+++ client/server/db/LoadThoughts.js
@@ -8,3 +8,7 @@ AWS.config.update({
     endpoint: "http://localhost:8000"
 });
 const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+//fs package to read users.json and assign to allUsers
+console.log("Importing thoughts into DynamoDB. Please wait.");
+const allUsers = JSON.parse(fs.readFileSync('./server/seed/users.json', 'utf8'));
