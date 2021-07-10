530fbd5b6af1ee7df24d656c822a319272274353
Ben Flynn <flynn0087@gmail.com>
2021-07-10 12:33:32 -0500

    loop over allUsers and create params, call to database added

--- client/server/db/LoadThoughts.js
+++ client/server/db/LoadThoughts.js
@@ -12,3 +12,24 @@ const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 //fs package to read users.json and assign to allUsers
 console.log("Importing thoughts into DynamoDB. Please wait.");
 const allUsers = JSON.parse(fs.readFileSync('./server/seed/users.json', 'utf8'));
+
+//loop over allUsers and create params
+allUsers.forEach(user => {
+    const params = {
+      TableName: "Thoughts",
+      Item: {
+        "username": user.username,
+        "createdAt": user.createdAt,
+        "thought": user.thought
+      }
+    };
+
+    //call to database
+    dynamodb.put(params, (err, data) => {
+        if (err) {
+          console.error("Unable to add thought", user.username, ". Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+          console.log("PutItem succeeded:", user.username);
+        }
+    });
+});    
