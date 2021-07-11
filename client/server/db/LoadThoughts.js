f1545f622496a3bfc604f31c8d9a4c9706f4d7f4
Ben Flynn <flynn0087@gmail.com>
2021-07-11 12:36:25 -0500

    removed localhost endpoint

--- client/server/db/LoadThoughts.js
+++ client/server/db/LoadThoughts.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 //dyanmoDB config
 AWS.config.update({
     region: "us-east-2",
-    endpoint: "http://localhost:8000"
 });
 const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 
