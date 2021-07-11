c19ec38cfb0e78e46e9ed0c86408d683a308e77d
Ben Flynn <flynn0087@gmail.com>
2021-07-11 14:21:59 -0500

    params-config.js details for images

---
+++ client/server/utils/params-config.js
@@ -0,0 +1,17 @@
+//dependencies
+const { v4: uuidv4 } = require('uuid');
+
+const params = fileName => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+  
+    const imageParams = {
+      Bucket: 'user-images-e015ead4-9896-4d7b-909a-0f26976d8a1e',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer
+    };
+  
+    return imageParams;
+};
+
+module.exports = params;
